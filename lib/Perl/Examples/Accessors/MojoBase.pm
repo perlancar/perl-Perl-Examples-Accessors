40b5cf55126a4c843b8a13ff1d690b9183c6a85b
perlancar (@pc-home) <perlancar@gmail.com>
2016-04-14 10:52:29 +0700

    Add classes: Perl::Examples::Accessors::{MojoBase,ObjectSimple}

---
+++ lib/Perl/Examples/Accessors/MojoBase.pm
@@ -0,0 +1,27 @@
+package Perl::Examples::Accessors::MojoBase;
+
+# DATE
+# VERSION
+
+use Mojo::Base -base;
+
+has 'attr1';
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
+
+This is an example of a class which uses L<Mojo::Base>.
+
+
+=head1 ATTRIBUTES
+
+=head2 attr1
+
+
+=head1 METHODS
+
+=head2 new() => obj
+
+Constructor.
---
+++ lib/Perl/Examples/Accessors/ObjectSimple.pm
@@ -0,0 +1,27 @@
+package Perl::Examples::Accessors::ObjectSimple;
+
+# DATE
+# VERSION
+
+use Object::Simple -base;
+
+has 'attr1';
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
+
+This is an example of a class which uses L<Object::Simple>.
+
+
+=head1 ATTRIBUTES
+
+=head2 attr1
+
+
+=head1 METHODS
+
+=head2 new() => obj
+
+Constructor.
