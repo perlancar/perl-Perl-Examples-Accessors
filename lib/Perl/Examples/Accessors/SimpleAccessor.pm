2c8b9f485a0352aff367050018f892be627a53ae
perlancar (@pc-home) <perlancar@gmail.com>
2016-06-26 16:29:06 +0700

    Add class: Perl::Examples::Accessors::SimpleAccessor

---
+++ lib/Perl/Examples/Accessors/SimpleAccessor.pm
@@ -0,0 +1,25 @@
+package Perl::Examples::Accessors::SimpleAccessor;
+
+# DATE
+# VERSION
+
+use Simple::Accessor qw(attr1);
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
+
+This is an example of a class which uses L<Simple::Accessor>. It is hash-based.
+
+
+=head1 ATTRIBUTES
+
+=head2 attr1
+
+
+=head1 METHODS
+
+=head2 new(%attrs) => obj
+
+Constructor. Accept a I<hashref> to set attributes.
