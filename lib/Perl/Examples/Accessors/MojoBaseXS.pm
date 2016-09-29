b41cf9898281b47da6b1889035316d627bac6d3e
perlancar (@pc-office) <perlancar@gmail.com>
2016-09-29 17:15:09 +0700

    Add class: Perl::Examples::Accessors::MojoBaseXS

---
+++ lib/Perl/Examples/Accessors/MojoBaseXS.pm
@@ -0,0 +1,27 @@
+package Perl::Examples::Accessors::MojoBaseXS;
+
+# DATE
+# VERSION
+
+use Mojo::Base::XS;
+
+has 'attr1';
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
+
+This is an example of a class which uses L<Mojo::Base::XS>.
+
+
+=head1 ATTRIBUTES
+
+=head2 attr1
+
+
+=head1 METHODS
+
+=head2 new() => obj
+
+Constructor.
