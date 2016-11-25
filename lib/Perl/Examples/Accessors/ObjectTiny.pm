3d50f3bae7f5683ec224f6556319540045566c6d
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-11-25 13:35:57 +0700

    Add class: Perl::Examples::Accessors::ObjectTiny

---
+++ lib/Perl/Examples/Accessors/ObjectTiny.pm
@@ -0,0 +1,25 @@
+package Perl::Examples::Accessors::ObjectTiny;
+
+# DATE
+# VERSION
+
+use Object::Tiny qw(attr1);
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
+
+This is an example of a class which uses L<Object::Tiny>. It is hash-based.
+
+
+=head1 ATTRIBUTES
+
+=head2 attr1
+
+
+=head1 METHODS
+
+=head2 new(%attrs) => obj
+
+Constructor. Accept a I<hash> to set attributes.
