591b4c74a7a1ea32987a217a6f1ad00e012e898b
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-10-15 21:05:15 +0700

    Add Perl::Examples::Accessors::ClassAccessorPackedStringSet

---
+++ lib/Perl/Examples/Accessors/ClassAccessorPackedStringSet.pm
@@ -0,0 +1,33 @@
+package Perl::Examples::Accessors::ClassAccessorPackedStringSet;
+
+# DATE
+# VERSION
+
+use Class::Accessor::PackedString::Set {
+    constructor => 'new',
+    accessors => {
+        attr1 => "f",
+    },
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
+
+This is an example of a class which uses L<Class::Accessor::PackedString::Set>.
+It is backed by a string.
+
+
+=head1 ATTRIBUTES
+
+=head2 attr1
+
+A float.
+
+
+=head1 METHODS
+
+=head2 new([ %attrs ]) => obj
+
+Constructor.
